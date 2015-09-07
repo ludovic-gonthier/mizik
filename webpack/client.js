a9079df8cdef72b9c198b773b76c7c31d73efe92
ludovic-gonthier <gonthier.ludovic@gmail.com>
2015-09-07 21:58:42 +0200

    chore(webpack): configure webpack to build files

---
+++ webpack/client.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import webpack from 'webpack';
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const ROOT = resolve(__dirname, '..');
+
+module.exports = {
+  debug: true,
+  devServer: {
+    contentBase: resolve(ROOT, 'build'),
+  },
+  devtool: 'eval',
+  entry: {
+    application: resolve(ROOT, 'src', 'module.js')
+  },
+  module: {
+    loaders: [
+      {
+        test : /\.(js|jsx)$/,
+        include :  resolve(ROOT, 'src'),
+        loaders : ['babel?optional[]=runtime']
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        loader: "file"
+      }
+    ]
+  },
+  output: {
+    path: resolve(ROOT, 'build'),
+    filename: "[name].js"
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: resolve(ROOT, 'src', 'views', 'index.html')
+    })
+  ],
+  resolve: {
+    root: resolve(ROOT, 'src'),
+    extensions : ['', '.js', '.jsx'],
+    alias: [
+      'actions',
+      'components',
+      'reducers',
+      'stores',
+      'views'
+    ]
+  },
+  target: 'web'
+};
